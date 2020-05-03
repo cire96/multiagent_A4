4007df5611fff9ab9c3846cca8cab4c89a649a8f
Eric Lind <ericl3@kth.se>
2020-05-03 14:49:09 +0200

    comment

--- BreakAI.py
+++ BreakAI.py
@@ -1,4 +1,64 @@
 import gym
+import tensorflow as tf
+import numpy as np
+
+class AtariWrapper():
+    def __init__(self,envID,no_random_shoot=10,n_history_step=4):
+        self.env = gym.make(envID)
+        self.state = None
+        self.last_lives = 0
+        self.no_random_shoot = no_random_shoot
+        self.n_history_step = n_history_step
+
+    def reset(self, evaluation=False):
+        observation = self.env.reset()
+        self.last_lives = 0
+        simulate_loss=True
+        if evaluation:
+            for i in range(np.random.randint(1, self.no_op_steps)):
+                observation = self.env.step(1) # Action 'Fire'
+        processedObs = self.ImageProcessor(observation)  
+        self.state = np.repeat(processedObs, self.agent_history_length, axis=2)
+        
+        return simulate_loss
+
+    def step(self,action):
+        new_observation, reward, real_loss, info = self.env.step(action)
+        reward=self.clip_reward(reward)
+
+        if info['ale.lives'] < self.last_lives:
+            simulate_loss = True
+        else:
+            simulate_loss = real_loss
+        self.last_lives = info['ale.lives']
+
+        processedObs=ImageProcessor(new_observation)
+        self.state = np.append(self.state[:, :, 1:], processedObs, axis=2)  
+
+        return processedObs, reward, real_loss, simulate_loss, new_observation  
+
+    def ImageProcessor(self, AleImage):
+        #frame dimensions same as used in deepmind
+        frame_height = 84
+        frame_width = 84
+
+        #grayScale
+        grayImage = tf.image.rgb_to_grayscale(AleImage)
+        #crop away unused part of the frame
+        grayImage = tf.image.crop_to_bounding_box(grayImage, 34, 0, 160, 160)
+        #downsize frame to 84x84
+        grayImage = tf.image.resize_images(grayImage,[frame_height, frame_width],  method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+        return grayImage
+    
+    # clip reward add this to atari wrapper(why not)
+    def clip_reward(self,reward):
+        if reward > 0:
+            return 1
+        elif reward == 0:
+            return 0
+        else:
+            return -1
+
 
 env = gym.make('Breakout-v0')
 print(env.action_space)
@@ -7,7 +67,6 @@ print(env.unwrapped.get_action_meanings())
 observation=env.reset()
 for _ in range(1000):
     env.render()
-    print(observation)
+    ob,reward,game_over=env.step(env.action_space.sample()) # take a random action
 
-    env.step(env.action_space.sample()) # take a random action
 env.close()
---
+++ newBreakNet.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+import numpy as np
+
+class BreakNet():
+    def __init__(self, n_actions, hidden=1024, learning_rate=0.00001,
+                 frame_height=84, frame_width=84, n_history_step=4):
+        
+        self.n_actions = n_actions
+        self.hidden = hidden
+        self.learning_rate = learning_rate
+        self.frame_height = frame_height
+        self.frame_width = frame_width
+        self.n_history_step = n_history_step
+
+        self.input = tf.compat.v1.placeholder(shape=[None, self.frame_height, self.frame_width, self.agent_history_length], dtype=tf.float32)
+
+        # Normalizing the input for memory
+        self.inputscaled = self.input/255
+
+        # Convolutional layers
+        self.conv1 = tf.compat.v1.layers.conv2d(
+            inputs=self.inputscaled, filters=32, kernel_size=[8, 8], strides=4,
+            kernel_initializer=tf.compat.v1.variance_scaling_initializer(scale=2),
+            padding="valid", activation=tf.nn.relu, use_bias=False, name='conv1')
+        self.conv2 = tf.compat.v1.layers.conv2d(
+            inputs=self.conv1, filters=64, kernel_size=[4, 4], strides=2, 
+            kernel_initializer=tf.compat.v1.variance_scaling_initializer(scale=2),
+            padding="valid", activation=tf.nn.relu, use_bias=False, name='conv2')
+        self.conv3 = tf.compat.v1.layers.conv2d(
+            inputs=self.conv2, filters=64, kernel_size=[3, 3], strides=1, 
+            kernel_initializer=tf.compat.v1.variance_scaling_initializer(scale=2),
+            padding="valid", activation=tf.nn.relu, use_bias=False, name='conv3')
+        self.conv4 = tf.compat.v1.layers.conv2d(
+            inputs=self.conv3, filters=hidden, kernel_size=[7, 7], strides=1, 
+            kernel_initializer=tf.compat.v1.variance_scaling_initializer(scale=2),
+            padding="valid", activation=tf.nn.relu, use_bias=False, name='conv4')
+
+        # Splitting into value and advantage stream
+        self.valuestream, self.advantagestream = tf.split(self.conv4, 2, 3)
+        self.valuestream = tf.compat.v1.layers.flatten(self.valuestream)
+        self.advantagestream = tf.compat.v1.layers.flatten(self.advantagestream)
+
+        #Split dense layers
+        self.advantage = tf.compat.v1.layers.dense(
+            inputs=self.advantagestream, units=self.n_actions,
+            kernel_initializer=tf.compat.v1.variance_scaling_initializer(scale=2), name="advantage")
+        self.value = tf.compat.v1.layers.dense(
+            inputs=self.valuestream, units=1, 
+            kernel_initializer=tf.compat.v1.variance_scaling_initializer(scale=2), name='value')
+
+        # Combining value and advantage into Q-values as described above
+        self.q_values = self.value + tf.subtract(self.advantage, tf.reduce_mean(input_tensor=self.advantage, axis=1, keepdims=True))
+        self.best_action = tf.argmax(input=self.q_values, axis=1)
+
+        # The next lines perform the parameter update. This will be explained in detail later.
+        
+        # targetQ according to Bellman equation: 
+        # Q = r + gamma*max Q', calculated in the function learn()
+        self.target_q = tf.compat.v1.placeholder(shape=[None], dtype=tf.float32)
+        # Action that was performed
+        self.action = tf.compat.v1.placeholder(shape=[None], dtype=tf.int32)
+        # Q value of the action that was performed
+        self.Q = tf.reduce_sum(input_tensor=tf.multiply(self.q_values, tf.one_hot(self.action, self.n_actions, dtype=tf.float32)), axis=1)
+        
+        # Parameter updates
+        self.loss = tf.reduce_mean(input_tensor=tf.compat.v1.losses.huber_loss(labels=self.target_q, predictions=self.Q))
+        self.optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=self.learning_rate)
+        self.update = self.optimizer.minimize(self.loss)
